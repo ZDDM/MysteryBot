9bf7a265154186559a5eb4a6e9a1250ee343b927
Zumo <zddm@outlook.es>
2017-08-16 23:45:11 +0200

    Added stuff :^)

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from mystery.game import Game
 description = """Murder everyone!"""
 token = ""
 game = None
-bot = commands.Bot(command_prefix=">", description=description)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('>'), description=description)
 
 @bot.command(description="Creates a game instance", pass_context=True)
 async def create_game(ctx):
+    global game
     if game == None and ctx.message.server:
-        await bot.say("%s created a game! Join using >join" %(ctx.message.author.mention))
-        global game
-        game = Game(bot, ctx.)
+        await bot.say("%s created a game! Join using >join and observe with >observe" %(ctx.message.author.mention))
+        game = Game(bot, ctx.message.server)
     elif game != None:
         await bot.say("A game already exists.")
     elif not ctx.message.server:
@@ -21,10 +21,34 @@ async def create_game(ctx):
     else:
         await bot.say("Unknown error...")
 
-@bot.command(description="Starts the game", pass_context=True)
-async def start(ctx):
-    """Puts the game in game mode."""
-    await bot.say("%s started the game!" %(ctx.message.author.mention))
+@bot.command(description="Stops a game instance", pass_context=True)
+async def stop(ctx):
+    await bot.say("Stopping game!")
+    game.stop()
+
+@bot.command(description="Join the game", pass_context=True)
+async def join(ctx):
+    if game:
+        game.add_player(ctx.message.author)
+
+@bot.command(description="Leave the game", pass_context=True)
+async def leave(ctx):
+    if game:
+        game.remove_player(ctx.message.author)
+
+@bot.command(description="Get a list of locations at reach", pass_context=True)
+async def locations(ctx):
+    if game:
+        if game.game_state == game.STATE_GAME:
+            bot.say("".join(game.locations))
+
+@bot.command(description="Move to another location", pass_context=True)
+async def move(ctx):
+    if game:
+        if game.game_state == game.STATE_GAME:
+            if ctx.subcommand_passed:
+                if game.locations.has_key(ctx.subcommand_passed):
+                    await game.locations[ctx.subcommand_passed].player_enter(game.find_by_user(ctx.message.author))
 
 if __name__ == "__main__":
     bot.run(token)
--- mystery/game.py
+++ mystery/game.py
@@ -14,6 +14,8 @@ class Game():
 
         self.game_state = self.STATE_LOBBY
         self.players = []
+        self.observers = []
+        self.locations = {"1" : Location(self, name="Dev Room 1", topic="It's"), "2" : Location(self, name="Dev Room 2", topic="The"), "3" : Location(self, name="Dev Room 3", topic="Nutshack")}
 
         self.player_role = await self.bot.create_role(self.server, name="Mystery Player")
         self.observer_role = await self.bot.create_role(self.serevr, name="Mystery Observer")
@@ -24,54 +26,132 @@ class Game():
 
         self.channel = await self.bot.create_channel(self.game.server, "mystery_lobby", everyone_perm, plyer_perm, observer_perm)
 
-        self.locations = []
+        await self.bot.edit_channel(self.channel, topic="Mystery game lobby.")
+        await self.bot.send_message(self.channel, "Game will start in 45 seconds. (BUT ACTUALLY 15 FOR DEBUG HEEEHEEEHEEE)")
+        await asyncio.sleep(15)
+        await start()
 
     async def start(self):
         self.game_state = self.STATE_GAME
-
-        self.bot.send_message(self.channel, "The game has started!")
+        await self.bot.send_message(self.channel, "The game has started! @everyone")
+        await self.bot.edit_channel(self.channel, topic="Mystery game lobby. The game has already started!")
+        await asyncio.sleep(5)
+        await self.bot.edit_channel(self.channel, topic="Mystery game lobby. The game has already started! You can discuss it here.")
+        await self.bot.edit_channel_permissions(self.channel, target=self.player_role, overwrite=discord.PermissionOverwrite(read_messages=False, send_messages=False))
+        for player in players:
+            locations.values()[0].player_enter(player)
 
     async def stop(self):
         await self.bot.delete_role(self.server, self.player_role)
         await self.bot.delete_role(self.server, self.observer_role)
+        await self.bot.delete_channel(self.channel)
         for location in self.locations:
-            location.delete()
+            await location.delete()
 
-    async def add_player(user):
+    async def add_player(self, user):
         if self.game_state == self.STATE_LOBBY:
-            self.players.append(user)
-            return
+            player = find_by_user(user)
+            if not (player in players):
+                self.players.append(Player(self, user))
+                self.bot.add_roles(self.game.server.get_member(user.id), self.player_role)
+                self.bot.send_message(self.channel, "%s joins the game!" % (user.name))
 
-    async def remove_player(user):
+    async def remove_player(self, user):
         if self.game_state == self.STATE_LOBBY:
-            self.players.remove(user)
-            return
+            player = find_by_user(user)
+            if player:
+                if not player.is_observer and player in players:
+                    self.players.remove(player)
+                    self.bot.remove_roles(player.member, self.player_role)
+                    self.bot.send_message(self.channel, "%s leaves the game..." % (user.name))
+
+    async def add_observer(self, user):
+        player = find_by_user(user)
+        if not player:
+            self.observers.append(Player(self, user, True))
+            self.bot.add_roles(self.game.server.get_member(user.id), self.observer_role)
+            return True
+        return False
+
+    async def remove_observer(self, user):
+        player = find_by_user(user)
+        if player:
+            if player.is_observer and player in observers:
+                self.observers.remove(player)
+                self.bot.remove_roles(player.member, self.observer_role)
+                return True
+        return False
+
+    def find_by_user(user):
+        for item in players:
+            if item.user == user:
+                return item
+        for item in observers:
+            if item.user == user:
+                return item
+        return False
+
+class Player():
+    async def __init__(self, game, user, is_observer=False):
+        self.game = game
+        self.user = user
+        self.member = self.game.server.get_member(self.user.id)
+        self.location = None
+        self.is_observer = is_observer
 
 class Location():
-    async def __init__(self, game, name):
+    async def __init__(self, game, name, topic=""):
         self.game = game
         self.name = name
         self.role = await self.game.bot.create_role(self.game.server, name="Location")
+
+        self.players = []
 
         everyone_perm = discord.ChannelPermissions(target=self.game.server.default_role, overwrite=discord.PermissionOverwrite(read_messages=False, send_messages=False))
         role_perm = discord.ChannelPermissions(target=self.role, overwrite=discord.PermissionOverwrite(read_messages=True, send_messages=True))
         observer_perm = discord.ChannelPermissions(target=self.game.observer_role, overwrite=discord.PermissionOverwrite(read_messages=True, send_messages=False))
 
         self.channel = await self.game.bot.create_channel(self.game.server, "mystery_%s"%self.name, everyone_perm, role_perm, observer_perm)
+        await self.game.bot.edit_channel(self.channel, topic=topic)
+        self.on_message = decorate(self.game.bot.event)
 
-    async def player_enter(user):
-        if not self.role in self.game.server.get_member(user.id).roles:
-            self.game.bot.add_roles(self.game.server.get_member(user.id), self.role)
-            self.game.bot.send_message(self.channel, "%s enters." %(user.name))
+    async def player_enter(self, player):
+        if player.is_observer:
+            return False
+        if not (player in players):
+            await self.game.bot.add_roles(player.member, self.role)
+            await self.game.bot.send_message(self.channel, "%s enters." %(player.user.name))
+            if player.location:
+                await player.location.player_leave(player)
+            player.location = self
+            players.append(player)
+            return True
 
-    async def player_leave(user):
-        if self.role in self.game.server.get_member(user.id).roles:
-            self.game.bot.add_roles(self.game.server.get_member(user.id), self.role)
-            self.game.bot.send_message(self.channel, "%s leaves." %(user.name))
+    async def player_leave(self, player):
+        if player in players:
+            await self.game.bot.remove_roles(player.member, self.role)
+            await self.game.bot.send_message(self.channel, "%s leaves." %(player.user.name))
+            if player.location == self:
+                player.location = None
+            players.remove(player)
 
     async def delete(self):
         await self.game.bot.delete_channel(self.channel)
         await self.game.bot.delete_role(self.game.server, self.role)
+
+    def bot(self):
+        return self.game.bot
+
+    async def on_message(message):
+        if message.channel == self.channel:
+            await self.game.bot.process_commands(message)
+            await asyncio.sleep(20)
+            try:
+                await self.game.bot.delete_message(message)
+            except:
+                pass
+        else:
+            await self.game.bot.process_commands(message)
 
 if __name__ == "__main__":
     raise Exception("Execute main.py instead.")
